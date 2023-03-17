2ceb4e732797a4a468293b1de59325b48dc8f03f
Lou-Theo <28760135+lou-theo@users.noreply.github.com>
2023-03-17 23:52:15 +0100

    feat(step 2): create generic dialog and use it to confirm team removing

--- src/app/features/team-stats/team-stats.component.ts
+++ src/app/features/team-stats/team-stats.component.ts
@@ -5,13 +5,14 @@ import { GameModel } from '@core/models/game.model';
 import { StatisticsModel } from '@core/models/statistics.model';
 import { TeamModel } from '@core/models/team.model';
 import { NbaService } from '@core/services/nba.service';
+import { GenericDialogComponent } from '@shared/components/generic-dialog/generic-dialog.component';
 import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-team-stats',
   templateUrl: './team-stats.component.html',
   styleUrls: ['./team-stats.component.css'],
-  imports: [NgIf, NgFor, RouterLink, AsyncPipe],
+  imports: [NgIf, NgFor, RouterLink, AsyncPipe, GenericDialogComponent],
   standalone: true,
 })
 export class TeamStatsComponent implements OnInit {
---
+++ src/app/shared/components/generic-dialog/generic-dialog.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-generic-dialog',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './generic-dialog.component.html',
+  styleUrls: ['./generic-dialog.component.css'],
+})
+export class GenericDialogComponent {
+  @ViewChild('dialog') dialog!: ElementRef<HTMLDialogElement>;
+  @Input() title: string | undefined;
+
+  show(): void {
+    this.dialog.nativeElement.showModal();
+  }
+
+  close(): void {
+    this.dialog.nativeElement.close();
+  }
+}
