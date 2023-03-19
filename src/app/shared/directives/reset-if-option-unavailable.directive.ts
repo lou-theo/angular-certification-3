e33df07d09ba223aadd52de73ae8082c0e2b59b2
Lou-Theo <28760135+lou-theo@users.noreply.github.com>
2023-03-19 22:16:55 +0100

    fix: detection change in reset-if-option-unavailable.directive

--- src/app/shared/directives/reset-if-option-unavailable.directive.ts
+++ src/app/shared/directives/reset-if-option-unavailable.directive.ts
@@ -9,7 +9,7 @@ export class ResetIfOptionUnavailableDirective implements AfterContentChecked {
   /** The value of the option that should be selected if the selected option is unavailable. */
   @Input() appResetIfOptionUnavailable = '';
 
-  get selectedOption(): HTMLOptionElement | null {
+  private get selectedOption(): HTMLOptionElement | null {
     return this.elementRef.nativeElement.selectedOptions[0];
   }
 
@@ -24,12 +24,16 @@ export class ResetIfOptionUnavailableDirective implements AfterContentChecked {
     if (availableOptions.some((option: HTMLOptionElement) => option.value === this.selectedOption?.value)) {
       return;
     }
+    setTimeout(() => this.resetInput());
+  }
+
+  private resetInput(): void {
     this.elementRef.nativeElement.value = this.appResetIfOptionUnavailable;
     if (this.ngModel) {
       // This is necessary because the ngModel directive does not update the value of the form control when the value of the select element changes.
       this.ngModel.control?.setValue(this.appResetIfOptionUnavailable);
-      // change detection is needed after the ngModel change
-      this.changeDetectorRef.detectChanges();
+      // in case the ngModel directive is used in a component that uses OnPush change detection, we need to manually mark the component for change detection.
+      this.changeDetectorRef.markForCheck();
     }
   }
 }
